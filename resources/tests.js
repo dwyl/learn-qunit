9b3b8dbc6ac39866a9388adecd440d4d269ad26c
nelsonic <nodecoder@gmail.com>
2013-11-24 07:41:52 +0000

    first test passes: timer is zero on init

--- resources/tests.js
+++ resources/tests.js
@@ -1,8 +1,8 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+test( "Stopwatch startTime is Zero when first initialze the app", function() {
+  equal( S.startTime === 0, true );
 });
 
-test( "deepEqual test", function() {
-  var obj = { fo: "bar" };
-  deepEqual( obj, { foo: "bar" }, "Two objects can be the same in value" );
-});
+// test( "deepEqual test", function() {
+//   var obj = { foo: "bar" };
+//   deepEqual( obj, { foo: "bar" }, "Two objects can be the same in value" );
+// });
